15ac920ed616226180463d90a94ce1830c81f371
Lukas Navickas <luk.navickas@gmail.com>
2016-02-07 19:59:50 +0000

    added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,12 @@ var socket = io();
 socket.on('connect', function() {
    console.log('connected to socket.io server!'); 
 });
+
+socket.on('message', function(message) {
+    console.log('New message');
+    console.log(message.text); // we saved object message in backend
+});
+
+socket.emit('message', { // on message do smth
+       text: 'Welcome to the chat application!'
+   }); 
